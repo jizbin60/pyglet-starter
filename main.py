52c6c020391d5eb71332aed99452eb98cc251560
jizbin60 <60397389+jizbin60@users.noreply.github.com>
2020-02-12 11:14:52 -0500

    i added new picture and text

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import pyglet # import the library
 win = pyglet.window.Window() # create the window
 
-# Create some text
-# label = pyglet.text.Label('Hello, world', x = 200, y = 200)
+#Create some text
+text = pyglet.text.Label('Hello, world', x = 200, y = 200)
+uno = pyglet.text.Label('Hola, world', font_size = 31, x = 300, y = 300)
+dos = pyglet.text.Label('Bonjour, world', font_size = 48, x = 400, y = 400)
+
 
 # Create a sprite
 img = pyglet.image.load('assets/hero/Old hero.png')
+door = pyglet.image.load('assets/hero/Old hero.png')
+cat = pyglet.image.load('assets/hero/Old hero.png')
+dog = pyglet.image.load('assets/hero/Old hero.png')
+goat = pyglet.image.load('assets/hero/Old hero.png')
+rat = pyglet.image.load('sheet.png')
 # spr = pyglet.sprite.Sprite(img, x=50, y=50)
 
 # Start the event loop
 def update(dt):
   pass
 
+
 @win.event
 def on_draw():
     win.clear()
-    img.blit(200, 100)
+    text.draw()
+    uno.draw()
+    dos.draw()
+    img.blit(0, 0)
+    door.blit(555, 222)
+    cat.blit(444, 123)
+    dog.blit(110,100)
+    goat.blit(198, 300)
+    rat.blit(-20, 380)
     #img.blit(400, 100)
     # spr.draw()
 
